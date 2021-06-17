96c80c27003d5b190582625908bace1ddba1f01c
Atahan Öztürk <ozturkatahan5@gmail.com>
2021-06-17 23:11:49 +0300

    db nesnesi

--- RegisterApp/Controllers/RegisterController.cs
+++ RegisterApp/Controllers/RegisterController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RegisterApp.DataAccess;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +9,15 @@ namespace RegisterApp.Controllers
 {
     public class RegisterController : Controller
     {
+        private readonly RegisterContext _db;
+
+        public RegisterController(RegisterContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+
             return View();
         }
     }
